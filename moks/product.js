d47d08c2aee0782b41027c6be02bc48999e6275e
Maxim <maxx5326@gmail.com>
2023-02-13 12:43:09 +0500

    Изменение названия в цене

--- moks/product.js
+++ moks/product.js
@@ -3,28 +3,28 @@ const products = [
         photoSrc : './img/HeadphoneSony.jpg',
         tittle : 'Наушники сони',
         desc : 'Крутые наушники',
-        price : '10000'
+        price : '10000 рублей'
     },
 
     {
         photoSrc : './img/HeadphoneJBL.jpg',
         tittle : 'Наушники JBL',
         desc : 'Обычные наушники',
-        price : '104100'
+        price : '104100 рублей'
     },
 
     {
         photoSrc : './img/PhoneXiaomi.jpg',
         tittle : 'Xiaomi',
         desc : 'Телефон',
-        price : '12000'
+        price : '12000 рублей'
     },
 
     {
         photoSrc : './img/SmartWatch.jpg',
         tittle : 'Smart Watch',
         desc : 'Часы',
-        price : '113000'
+        price : '113000 рублей'
     }
 ]
 
